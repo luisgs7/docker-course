8fcc5c6b32ff078026098211b640e3ade1a5948b
luisgs7 <luisgsilva270@gmail.com>
2021-09-18 15:59:28 -0300

    docker network

--- docker-terminal/comandos.sh
+++ docker-terminal/comandos.sh
@@ -22,7 +22,7 @@ sudo docker run -d -p 8080:80 nginx:1.19.4-alpine
 
 sudo docker run --name linux_alpine --rm -i -t alpine:3.12.1 sh
 
-sudo docker exec -it <id> bash
+sudo docker exec -it <id> sh
 
 sudo docker run --name "servidor_web" -d -p 8080:80 -e NGINX_ENTRYPOINT_QUIET_LOGS=1 
             -v "/home/luis/www/courses/udemy/docker-curso/volumes:/usr/share/nginx/html" nginx:1.19.4
@@ -30,3 +30,11 @@ sudo docker run --name "servidor_web" -d -p 8080:80 -e NGINX_ENTRYPOINT_QUIET_LO
 sudo docker build -f Dockerfile -t username/image_name:v1 .
 
 sudo docker run --name "servidor_web" -d -p 8080:80 servido_web:v1
+
+sudo docker run --rm alpine:3.12.1 sh -c "ifconfig"
+
+sudo docker network ls
+
+sudo docker network inspect <name>
+
+sudo docker run --name "container_1_bridge" -d alpine:3.12.1 sleep 5000
